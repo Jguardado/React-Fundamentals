cb2e0f997742ad14966e638078f1cb61f67db275
Juan <jguardado85@gmail.com>
2016-03-21 17:02:43 -0700

    adding playerOne and playerTwo

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,11 +6,13 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
-
+var PromptContainer = require('../containers/PromptContainer');
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' component={PromptContainer} />
     </Route>
   </Router>
 );
